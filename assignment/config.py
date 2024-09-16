8b9fe17d4b2f95c83600e9fc795c81e9ee394935
Krish Shah <2003kshah@gmail.com>
2024-09-16 13:09:30 -0400

    config file

--- assignment/config.py
+++ assignment/config.py
@@ -1,4 +1,4 @@
-WIFI_SSID = "your_wifi_ssid"
-WIFI_PASSWORD = "your_wifi_password"
+WIFI_SSID = "BU Guest (unencrypted)"
+WIFI_PASSWORD = ""
 FIREBASE_PROJECT_ID = "ec463-mini-project-6d206"
 FIREBASE_API_KEY = "AIzaSyAgqDhZbdVJmnxFw1c1oLQOt5xKPLg2cCM"
